a462e3c7bafd71561f3d651553de743ef2f56b9d
Jeanadoan <140124143+Jeanadoan@users.noreply.github.com>
2023-11-03 19:08:36 -0700

    Add files via upload

---
+++ duble/src/duble/wagfwa.java
@@ -0,0 +1,20 @@
+package duble;
+
+public class wagfwa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] A ={{1, 2, 3},{4,5,6},{7,8,9}};
+		
+		for (int i=0; i<A.length; i++) {
+			for (int j=0; j<A[i].length; j++) {
+				System.out.print ( A [i][j] + " ");
+			}
+	
+	
+			
+		//System.out.println();
+      }
+	}
+	
+}
